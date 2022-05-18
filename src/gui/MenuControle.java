bda64d332000758bd9c1cfc7d306662839a7188d
bononibononi <bononi2003@yahoo.com.br>
2022-05-18 06:39:07 -0300

    botão sobre

--- src/gui/MenuControle.java
+++ src/gui/MenuControle.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MenuControle implements Initializable {
 
@@ -13,6 +22,8 @@ public class MenuControle implements Initializable {
 	private MenuItem menuItemVendedor;
 	@FXML
 	private MenuItem menuItemDepartamentos;
+	@FXML
+	private MenuItem menuSobre;
 	
 	@FXML
 	public void onMenuItemVendedorAcao() {
@@ -23,7 +34,10 @@ public class MenuControle implements Initializable {
 	public void onMenuItemDepartamentosAcao() {
 		System.out.println("onMenuItemDepartamentosAcao");
 	}
-	
+	@FXML
+	public void onMenuItemSobreAcao() {
+		loadView("/gui/Sobre.fxml");
+	}
 	
 	
 	@Override
@@ -31,4 +45,22 @@ public class MenuControle implements Initializable {
 		
 	}
 
+	private void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Erro ao carregar a página de visualização", e.getMessage(), AlertType.ERROR);
+		}
+	
+	}
 }
